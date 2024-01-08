53fd8b0e5b82c28ab8268302799ed6a5e76e4328
Indhu-siva <156095163+Indhu-siva@users.noreply.github.com>
2024-01-08 17:53:01 +0530

    Added new files

---
+++ Fake-review-monitoring/admin/include/footer.php
@@ -0,0 +1,8 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy;  Fake Product Review Monitoring</b> All rights reserved.
+			kathir mobiles,malumachinpatti
+		</div>
+	</div>
